ba38b50d6031ca37175a28bbb7035d858f0b2fe2
Unknown <JulZimmermann@gmail.com>
2018-06-06 13:16:02 +0200

    Added web project

---
+++ NAA.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NAA.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
