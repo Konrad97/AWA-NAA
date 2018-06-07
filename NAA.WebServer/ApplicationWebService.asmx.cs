0703e6fb5706290cadb0dab0f3837e4801815e58
Konrad <melzerkonrad1@gmail.com>
2018-06-07 20:34:09 +0200

    Ws Quickfix

--- NAA.WebServer/ApplicationWebService.asmx.cs
+++ NAA.WebServer/ApplicationWebService.asmx.cs
@@ -1,30 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.Web.Services;
 using System.Web.Services.Protocols;
-using System.Xml;
 using NAA.Service;
 using NAA.Shared.Model;
 using NAA.Shared.Service;
 
 namespace NAA.WebServer
 {
-
     /// <summary>
-    /// With this webservice you can check applications to your university and manage their state.
+    ///     With this webservice you can check applications to your university and manage their state.
     /// </summary>
-    
     [WebService(Namespace = "http://tempuri.org/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
-    [System.ComponentModel.ToolboxItem(false)]
+    [ToolboxItem(false)]
 
     // Wenn der Aufruf dieses Webdiensts aus einem Skript zulässig sein soll, heben Sie mithilfe von ASP.NET AJAX die Kommentarmarkierung für die folgende Zeile auf. 
     // [System.Web.Script.Services.ScriptService]
-
-    public class ApplicationWebService : System.Web.Services.WebService
+    public class ApplicationWebService : WebService
     {
-
-        readonly IApplicationService _service = new ApplicationService();
+        private readonly IApplicationService _service = new ApplicationService();
 
         [WebMethod]
         public List<Application> GetApplications(string university)
@@ -35,30 +30,20 @@ namespace NAA.WebServer
         [WebMethod]
         public Application SetApplicationsStatus(string university, int applicationId, OfferState offerState)
         {
-            Application application;
-            try
-            {
-                 application = _service.GetApplication(applicationId);
-            }
-            catch (ArgumentNullException e)
-            {
+            var application = _service.GetApplication(applicationId);
+
+            if (application == null)
                 throw new SoapException("Application was not Found.", SoapException.ClientFaultCode);
-            }
 
             if (application.University != university)
-            {
                 throw new SoapException("Permission Denied.", SoapException.ClientFaultCode);
-            }
 
             if (application.OfferState != OfferState.Conditional || application.OfferState != OfferState.Pending)
-            {
                 throw new SoapException("Offerstate can not be modified.", SoapException.ClientFaultCode);
-            }
 
             if (application.OfferState == OfferState.Conditional && offerState != OfferState.Unconditional)
-            {
-                throw new SoapException("Conditional Offer State can only be set to Unconditional.", SoapException.ClientFaultCode);
-            }
+                throw new SoapException("Conditional Offer State can only be set to Unconditional.",
+                    SoapException.ClientFaultCode);
 
             application.OfferState = offerState;
             _service.EditApplication(application);
